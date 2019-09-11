cecd50d5200edc51ecd639394a1b54b6f8681112
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-11 00:08:59 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Operators
+irb# Operators
 
 ## Objectives
 
--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+if speed < 40 || speed > 60
+  puts "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+else puts "you're within the speed limits"
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 40 || speed > 60 ? puts "Going 30 mph on the freeway would be unsafe, as would going 95 mph." : puts "you're within the speed limits"
 end
 	
 
