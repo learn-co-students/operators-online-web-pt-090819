48add2b923bcfe85259507b26e34775c8f42252a
Abraham Galva <ag212@icloud.com>
2019-09-16 21:19:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
-  if speed < 40
+  if speed <40
     return true
-  elsif speed > 60
+  elsif speed >60
   return true
   else
     return false
@@ -9,5 +9,5 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	speed < 40 || speed >60 ? true : false
+	speed <40 || speed >60 ? true : false
 end
