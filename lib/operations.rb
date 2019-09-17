986156159ba0dcdf72ea46266899e850726983a5
Janay Bailey <tjanay87@gmail.com>
2019-09-17 18:41:44 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
-def unsafe?(speed)
+speed = ""
 
+def unsafe?(speed)
+if speed < 40 ? "Going 30 mph on the freeway would be unsafe " :
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 30 ? "That's within a safe range" :
 end
 	
 
